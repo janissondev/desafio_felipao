6eb40a9ed1f5ee9647a2eb71129ca839bdfbaefa
janissondev <janisson_matos10@hotmail.com>
2023-10-24 10:29:08 -0300

    Declaração de váriaveis e loop de repetição

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-let nome = Carlos
+
+let nome = prompt("Olá, herói! Informe o seu nome:");
+let desafios = Number(prompt("Muito bom ver você por aqui, " + nome + ". Agora nos informe quantos desafios você resolveu (cada desafio vale 200pts)"));
+let pontosXP = 0 
+
+for(contador = 1; contador <= desafios; contador++) {
+pontosXP += 200
+}
+
+
